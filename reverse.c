7b4bc9001770a2374973fd1ded9db708203c6811
LinkinYoung <1143052146@qq.com>
2015-10-12 17:49:23 +0800

    reverse Exercise 1-19 completed.

--- reverse.c
+++ reverse.c
@@ -8,14 +8,28 @@
 
 #include <stdio.h>
 
+void reverse(char *s,int length){
+    int i,j=0;
+    char temp[100]={0};
+    for (i=0; i<length; i++) {
+        temp[i] = s[i];
+    }
+    for (i=length-1; i>=0; i--) {
+        s[i] = temp[j];
+        j++;
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    int i;
+    int i,num;
     char c;
     char ch[100]={0};
     for (i=0; (c = getchar()) != '\n'; i++) {
         ch[i] = c;
     }
-    for (i--; i>=0; i--) {
+    num = i;
+    reverse(ch, num);
+    for (i=0; i<num; i++) {
         putchar(ch[i]);
     }
     putchar('\n');
