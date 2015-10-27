31a51636eed3e7b94ecd1b921024185561cd2537
LinkinYoung <1143052146@qq.com>
2015-10-27 23:43:49 +0800

    Exercise2-3 Not using library

--- htoi.c
+++ htoi.c
@@ -8,10 +8,24 @@
 
 #include <stdio.h>
 
+int numof(char c){
+    //用这个函数求字母代表的数字
+    if (c <='9') {
+        return c-'0';
+    } else if (c > 'Z'){
+        return c-'a'+10;
+    }else {
+        return c-'A'+10;
+    }
+}
 int htoi(char *s){
-    //将字符串中的16进制数转换为整型。调用sscanf函数
-    int n;
-    sscanf(s, "%x",&n);
+    //从后往前扫描，遇到x或者i<0停止
+    int n=0,i,j=0;
+    for (i=0; s[i]!='\0'; i++);
+    for (i--; (i>=0) && (s[i]!='x') && (s[i]!='X'); i--) {
+        n+=numof(s[i])*(1<<(4*j));//用左移进行乘方运算
+        j++;
+    }
     return n;
 }
 int main(int argc, const char * argv[]) {
